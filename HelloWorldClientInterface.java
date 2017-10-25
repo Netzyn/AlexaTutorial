16e27b13e942ff39a2e904d53c168a374cd5fac7
Stephen James <sajames1958@gmail.com>
2017-10-25 09:22:52 -0500

    Added get logging tag function.

--- HelloWorldClientInterface.java
+++ HelloWorldClientInterface.java
@@ -1,10 +1,11 @@
 package com.netzyn.example;
 
 /**
- * Created by Stephen on 7/28/2017.
+ * 
  */
 
 public interface HelloWorldClientInterface {
 
     void HelloWorld(String sessionId);
+    String GetLoggingTag(); 
 }
